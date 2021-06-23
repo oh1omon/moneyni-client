48cf617bcf706b9b124d4aa5acae4384d5fe2678
Artem Zagarov <artem.zagarov@gmail.com>
2021-06-23 14:34:13 +0300

    sterted work with headrooom

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -10,11 +10,14 @@ export const Header = (): JSX.Element => {
 		location.pathname === '/home' ? setAdvanced(true) : setAdvanced(false)
 	}, [location])
 	return (
-		<Headroom className='bg-main-dark'>
+		<Headroom
+		// style={{ backgroundColor: 'rgb(66, 92, 90)', height: `${advanced ? 'h-screen-1/2' : 'h-screen-1/10'}` }}
+		// // className={` transition-all  transition-300 ease-in-out`}
+		>
 			<div
-				className={`w-full ${
-					advanced ? 'h-screen-1/2' : 'h-screen-1/10'
-				}  transition-300 ease-in-out bg-main-light rounded-b-2xl`}
+			// className={`w-full ${
+			// 	advanced ? 'h-screen-1/2' : 'h-screen-1/10'
+			// } transition-all  transition-300 ease-in-out bg-main-light rounded-b-2xl`}
 			>
 				f
 			</div>
