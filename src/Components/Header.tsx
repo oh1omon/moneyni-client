5ccffcb9fd860740fc97d06625982bba6639c011
Artem Zagarov <artem.zagarov@gmail.com>
2021-06-23 20:09:19 +0300

    added header version for others screens

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -19,45 +19,52 @@ export const Header = (): JSX.Element => {
 				advanced ? 'h-screen-4.5/10' : 'h-screen-1/10'
 			} transition-all  transition-300 ease-in-out bg-main-dark`}
 		>
-			<div className={'w-full h-full bg-main-light rounded-b-2xl flex justify-center items-center'}>
-				<div className={'h-4/5 w-5/6 grid grid-rows-11 text-white'}>
-					<div className={'row-start-1 row-span-5 flex justify-center items-center'}>
-						<div className='flex flex-col h-full justify-around'>
-							<p>Current balance</p>
-							<h2 className='text-4xl'>{salarySplitter(user!.salary.actual)} €</h2>
-						</div>
-					</div>
-					<div className={'row-start-6 row-span-3'}>
-						<div className='flex h-full justify-between'>
-							<div className='w-1/3 flex flex-col justify-around'>
-								<p>Income</p>
-								<h3 className='text-2xl'>{salarySplitter(user!.salary.monthly)} €</h3>
-							</div>
-							<div className='w-1/3 flex flex-col justify-around'>
-								<p>Spent</p>
-								<h3 className='text-2xl'>
-									{salarySplitter(user!.salary.monthly - user!.salary.actual)} €
-								</h3>
+			<div className={'w-full h-full bg-main-light rounded-b-3xl flex justify-center items-center'}>
+				{advanced ? (
+					<div className={'h-4/5 w-5/6 grid grid-rows-11 text-white'}>
+						<div className={'row-start-1 row-span-5 flex justify-center items-center'}>
+							<div className='flex flex-col h-full justify-around'>
+								<p>Current balance</p>
+								<h2 className='text-4xl'>{salarySplitter(user!.salary.actual)} €</h2>
 							</div>
 						</div>
-					</div>
-					<div className='relative month row-start-10 row-span-3 max-w-full h-full flex justify-around items-center overflow-y-auto'>
-						<div className='w-auto flex justify-between items-center overflow-y-auto'>
-							<p className='px-2 py-1 mx-4 rounded-lg bg-main-dark'>January</p>
-							<p className='px-2 py-1 mx-4 rounded-lg bg-main-dark'>February</p>
-							<p className='px-2 py-1 mx-4 rounded-lg bg-main-dark'>March</p>
-							<p className='px-2 py-1 mx-4 rounded-lg bg-main-dark'>April</p>
-							<p className='px-2 py-1 mx-4 rounded-lg bg-main-dark'>May</p>
-							<p className='px-2 py-1 mx-4 rounded-lg bg-main-dark'>June</p>
-							<p className='px-2 py-1 mx-4 rounded-lg bg-main-dark'>July</p>
-							<p className='px-2 py-1 mx-4 rounded-lg bg-main-dark'>August</p>
-							<p className='px-2 py-1 mx-4 rounded-lg bg-main-dark'>September</p>
-							<p className='px-2 py-1 mx-4 rounded-lg bg-main-dark'>October</p>
-							<p className='px-2 py-1 mx-4 rounded-lg bg-main-dark'>November</p>
-							<p className='px-2 py-1 mx-4 rounded-lg bg-main-dark'>December</p>
+						<div className={'row-start-6 row-span-3'}>
+							<div className='flex h-full justify-between'>
+								<div className='w-1/3 flex flex-col justify-around'>
+									<p>Income</p>
+									<h3 className='text-2xl'>{salarySplitter(user!.salary.monthly)} €</h3>
+								</div>
+								<div className='w-1/3 flex flex-col justify-around'>
+									<p>Spent</p>
+									<h3 className='text-2xl'>
+										{salarySplitter(user!.salary.monthly - user!.salary.actual)} €
+									</h3>
+								</div>
+							</div>
+						</div>
+						<div className='relative month row-start-10 row-span-3 max-w-full h-full flex justify-around items-center overflow-y-auto'>
+							<div className='w-auto flex justify-between items-center overflow-y-auto'>
+								<p className='px-2 py-1 mx-4 rounded-lg bg-main-dark'>January</p>
+								<p className='px-2 py-1 mx-4 rounded-lg bg-main-dark'>February</p>
+								<p className='px-2 py-1 mx-4 rounded-lg bg-main-dark'>March</p>
+								<p className='px-2 py-1 mx-4 rounded-lg bg-main-dark'>April</p>
+								<p className='px-2 py-1 mx-4 rounded-lg bg-main-dark'>May</p>
+								<p className='px-2 py-1 mx-4 rounded-lg bg-main-dark'>June</p>
+								<p className='px-2 py-1 mx-4 rounded-lg bg-main-dark'>July</p>
+								<p className='px-2 py-1 mx-4 rounded-lg bg-main-dark'>August</p>
+								<p className='px-2 py-1 mx-4 rounded-lg bg-main-dark'>September</p>
+								<p className='px-2 py-1 mx-4 rounded-lg bg-main-dark'>October</p>
+								<p className='px-2 py-1 mx-4 rounded-lg bg-main-dark'>November</p>
+								<p className='px-2 py-1 mx-4 rounded-lg bg-main-dark'>December</p>
+							</div>
 						</div>
 					</div>
-				</div>
+				) : (
+					<div className='w-4/5 h-4/5 flex justify-between items-center text-white text-2xl'>
+						<p>{user?.name}</p>
+						<p>MoneyNi</p>
+					</div>
+				)}
 			</div>
 		</div>
 	)
